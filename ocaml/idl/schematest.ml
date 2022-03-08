0903eaaefa68eee1e805894a8699ad95f1cfdd91
Pau Ruiz Safont <pau.safont@citrix.com>
2022-03-08 12:01:53 +0000

    CP-38626: Allow manipulation of VTPM contents
    This is restricted to local access only, through the UNIX socket.
    
    This allows the software TPM daemon to retrieve and push the contents of
    the TPM from and to the xapi database.
    
    Any remote accesses to the contents are forbidden,
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -1,7 +1,7 @@
 let hash x = Digest.string x |> Digest.to_hex
 
 (* BEWARE: if this changes, check that schema has been bumped accordingly *)
-let last_known_schema_hash = "8ae84546b95daaeef2bd0639c734e9e8"
+let last_known_schema_hash = "e94081da897f9ea58b4aafa43c181ce4"
 
 let current_schema_hash : string =
   let open Datamodel_types in
